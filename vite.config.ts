020635b9cee5a06d8fb1d5a455e4a07d16bb2e59
Lars Salembier <lars.salembier@gmail.com>
2023-05-09 16:05:57 +0200

    more upgrades to linting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+
+import react from '@vitejs/plugin-react-swc';
+import { reactScopedCssPlugin } from 'rollup-plugin-react-scoped-css';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths(), reactScopedCssPlugin()],
+  base: '',
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/styles/variables.scss";`,
+      },
+    },
+  },
 });
